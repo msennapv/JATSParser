a1f92d3a21e65e49ab89c14b3041d6d9c9be0697
msennapv <msennapv@users.noreply.github.com>
2020-08-26 19:41:19 +0200

    Aggiunto link generato automaticamente a Google Scholar per ogni referenza

--- src/JATSParser/HTML/ReferenceMethods.php
+++ src/JATSParser/HTML/ReferenceMethods.php
@@ -92,6 +92,54 @@ class ReferenceMethods {
 					}
 				}
 			}
+		
+			// by Massimo Senna 
+			// aggiungo link a googlescholar 
+			// https://scholar.google.com/scholar_lookup?author=E+Lefran%C3%A7ais&author=G+Ortiz-Mu%C3%B1oz&author=A+Caudrillier&title=The+lung+is+a+site+of+platelet+biogenesis+and+a+reservoir+for+haematopoietic+progenitors&publication_year=2017&journal=Nature&volume=544&pages=105-109
+
+			// if ($number > 0) {
+			// 	$pmidDelimeter = $domElement->ownerDocument->createTextNode(' | ');
+			// 	$domElement->appendChild($pmidDelimeter);
+			// }
+			$gsidLink = $domElement->ownerDocument->createElement('a');
+			$scholarUrl='https://scholar.google.com/scholar_lookup';
+			$auth_cnt=0;
+			foreach ($jatsReference->getAuthors() as $key => $author) {
+				if (get_class($author) === "JATSParser\Back\Individual") {
+					$a=$author->getSurname().' '.$author->getGivenNames();
+					if($a){
+						if($auth_cnt==0){
+							$scholarUrl.='?author='.$a;
+						}else{
+							$scholarUrl.='&author='.$a;
+						}
+					}
+					$auth_cnt+=1;
+				} elseif (get_class($author) === "JATSParser\Back\Collaboration") {
+					$a=$author->getName();
+					if($a){
+						if($auth_cnt==0){
+							$scholarUrl.='?author='.$a;
+						}else{
+							$scholarUrl.='&author='.$a;
+						}
+					}
+
+					$auth_cnt+=1;
+				}
+			}
+			if($auth_cnt==0){
+				$scholarUrl.='?title=';
+			}else{
+				$scholarUrl.='&title=';
+			}
+
+			$scholarUrl.=$jatsReference->getTitle();
+
+			$scholarUrl=str_replace(' ','+',$scholarUrl);
+			$gsidLink->setAttribute("href", $scholarUrl);
+			$gsidLink->nodeValue = "Google Scholar";
+			$domElement->appendChild($gsidLink);
 	}
 
 	public static function extractEditors(AbstractReference $jatsReference, \DOMElement $domElement) {
