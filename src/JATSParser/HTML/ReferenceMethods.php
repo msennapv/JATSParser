30bb7a8c26e60838a1bdac09b98ae1d319e38805
msennapv <msennapv@users.noreply.github.com>
2020-09-05 19:33:01 +0200

    Update ReferenceMethods.php
    Aggiunta gestione pmid e pmcid esterni, corretta gestione separatore

--- src/JATSParser/HTML/ReferenceMethods.php
+++ src/JATSParser/HTML/ReferenceMethods.php
@@ -51,48 +51,160 @@ class ReferenceMethods {
 				$urlLink->nodeValue = "Publisher Full Text";
 				$domElement->appendChild($urlLink);
 
-				$delimeter = $domElement->ownerDocument->createTextNode(' | ');
-				$domElement->appendChild($delimeter);
+				// $delimeter = $domElement->ownerDocument->createTextNode(' | ');
+				// $domElement->appendChild($delimeter);
 			} elseif ($jatsReference->getUrl() !== '') {
 				$urlLink = $domElement->ownerDocument->createElement('a');
 				$urlLink->setAttribute("href", $jatsReference->getUrl());
 				$urlLink->nodeValue = "Publisher Full Text";
 				$domElement->appendChild($urlLink);
 			}
-
+			$doifound=false;
+			$pmidfound=false;
+			$pmcidfound=false;
 			if ($jatsReference->getPubIdType() !== ''){
 				$number = 0;
 				foreach ($jatsReference->getPubIdType() as $key => $pubId) {
 					if ($key == 'doi') {
+						// if ($number > 0) {
+						// 	$pmidDelimeter = $domElement->ownerDocument->createTextNode(' | ');
+						// 	$domElement->appendChild($pmidDelimeter);
+						// }
 						$number += 1;
 						$doiLink = $domElement->ownerDocument->createElement('a');
 						$doiLink->setAttribute("href", $pubId);
-						$doiLink->nodeValue = "DOI";
+						$doiLink->setAttribute("target", '_blank'); //aggiunto
+						// guidelines CrossRef
+						$doiLink->nodeValue = $pubId;
+						//$doiLink->nodeValue = "CrossRef";
 						$domElement->appendChild($doiLink);
+						$doifound=true;	//aggiunto per gestione doi dalle referenze se necessario
 					} elseif ($key == "pmid") {
-						if ($number > 0) {
-							$pmidDelimeter = $domElement->ownerDocument->createTextNode(' | ');
-							$domElement->appendChild($pmidDelimeter);
-						}
+						// if ($number > 0) {
+						// 	$pmidDelimeter = $domElement->ownerDocument->createTextNode(' | ');
+						// 	$domElement->appendChild($pmidDelimeter);
+						// }
 						$number += 1;
 						$pmidLink = $domElement->ownerDocument->createElement('a');
 						$pmidLink->setAttribute("href", $pubId);
+						$pmidLink->setAttribute("target", '_blank'); //aggiunto
 						$pmidLink->nodeValue = "PubMed";
 						$domElement->appendChild($pmidLink);
+						$pmidfound=true;	//aggiunto per gestione pmid dalle referenze se necessario
 					} elseif ($key == "pmcid") {
-						if ($number > 0) {
-							$pmidDelimeter = $domElement->ownerDocument->createTextNode(' | ');
-							$domElement->appendChild($pmidDelimeter);
-						}
+						// if ($number > 0) {
+						// 	$pmidDelimeter = $domElement->ownerDocument->createTextNode(' | ');
+						// 	$domElement->appendChild($pmidDelimeter);
+						// }
 						$number += 1;
 						$pmcidLink = $domElement->ownerDocument->createElement('a');
 						$pmcidLink->setAttribute("href", $pubId);
+						$pmcidLink->setAttribute("target", '_blank'); //aggiunto
 						$pmcidLink->nodeValue = "PubMed Central";
-
+						$domElement->appendChild($pmcidLink);
+						$pmcidfound=true;	//aggiunto per gestione pmcid dalle referenze se necessario
 					}
 				}
+
 			}
+
+			// by Massimo Senna 
+			// aggiungo tag provvisorio doi,pmid,pmcid per provare a ricavarlo dalle referenze di OJS  
+
+
+			// se non ho trovato il doi e/o pmid e/o pmcid nel file xml, aggiungi dei segnaposto da usare dopo
+			if(!$doifound){
+				// if ($number > 0) {
+				// 	$pmidDelimeter = $domElement->ownerDocument->createTextNode(' | ');
+				// 	$domElement->appendChild($pmidDelimeter);
+				// }
+						$number += 1;
+						$doiLink = $domElement->ownerDocument->createElement('extdoi');
+						$doiLink->setAttribute("id", 'doi'.$jatsReference->getId());
+						$domElement->appendChild($doiLink);
+			}
+			if(!$pmidfound){
+				// if ($number > 0) {
+				// 	$pmidDelimeter = $domElement->ownerDocument->createTextNode(' | ');
+				// 	$domElement->appendChild($pmidDelimeter);
+				// }
+						$number += 1;
+						$doiLink = $domElement->ownerDocument->createElement('extpmid');
+						$doiLink->setAttribute("id", 'pmid'.$jatsReference->getId());
+						$domElement->appendChild($doiLink);
+			}
+			if(!$pmcidfound){
+				// if ($number > 0) {
+				// 	$pmidDelimeter = $domElement->ownerDocument->createTextNode(' | ');
+				// 	$domElement->appendChild($pmidDelimeter);
+				// }
+						$number += 1;
+						$doiLink = $domElement->ownerDocument->createElement('extpmcid');
+						$doiLink->setAttribute("id", 'pmcid'.$jatsReference->getId());
+						$domElement->appendChild($doiLink);
+			}
+			// by Massimo Senna 
+			// aggiungo link a googlescholar 
+			// https://scholar.google.com/scholar_lookup?author=E+Lefran%C3%A7ais&author=G+Ortiz-Mu%C3%B1oz&author=A+Caudrillier&title=The+lung+is+a+site+of+platelet+biogenesis+and+a+reservoir+for+haematopoietic+progenitors&publication_year=2017&journal=Nature&volume=544&pages=105-109
+
+			// if ($number > 0) {
+			// 	$pmidDelimeter = $domElement->ownerDocument->createTextNode(' | ');
+			// 	$domElement->appendChild($pmidDelimeter);
+			// }
+
+			$gsidLink = $domElement->ownerDocument->createElement('a');
+			$scholarUrl='https://scholar.google.com/scholar_lookup';
+			$auth_cnt=0;
+			foreach ($jatsReference->getAuthors() as $key => $author) {
+				if (get_class($author) === "JATSParser\Back\Individual") {
+					$a=$author->getSurname().' '.$author->getGivenNames();
+					if($a){
+						if($auth_cnt==0){
+							$scholarUrl.='?author='.$a;
+						}else{
+							$scholarUrl.='&author='.$a;
+						}
+					}
+					$auth_cnt+=1;
+				} elseif (get_class($author) === "JATSParser\Back\Collaboration") {
+					$a=$author->getName();
+					if($a){
+						if($auth_cnt==0){
+							$scholarUrl.='?author='.$a;
+						}else{
+							$scholarUrl.='&author='.$a;
+						}
+					}
+
+					$auth_cnt+=1;
+				}
+			}
+			if($auth_cnt==0){
+				$scholarUrl.='?title=';
+			}else{
+				$scholarUrl.='&title=';
+			}
+
+			$scholarUrl.=$jatsReference->getTitle();
+
+			$scholarUrl=str_replace(' ','+',$scholarUrl);
+			// aggiungo una classe per il link scholar
+			$gsidLink->setAttribute("class", "artref-scholar-link");
+			$gsidLink->setAttribute("href", $scholarUrl);
+			$gsidLink->setAttribute("target", '_blank'); //aggiunto
+			$gsidLink->nodeValue = "Google Scholar";
+
+			// $pmidDelimeter = $domElement->ownerDocument->createElement('span');
+			// $pmidDelimeter->nodeValue='|';
+			// $pmidDelimeter->setAttribute("class", 'references-link-delimeter');
+			// $domElement->appendChild($pmidDelimeter);
+
+			$domElement->appendChild($gsidLink);
+
 	}
+
+	
+
 
 	public static function extractEditors(AbstractReference $jatsReference, \DOMElement $domElement) {
 		foreach ($jatsReference->getEditors() as $key => $author) {
